10e9896cbbd34bbc0fbbfde52fd3ea01b99c5ff0
hubodk <hubert2012201@gmail.com>
2023-03-23 14:59:35 +0100

    opdatering af spil, nye designs samt andet.

--- Tower Defense EKSAMEN/Scripts/World.gd
+++ Tower Defense EKSAMEN/Scripts/World.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var tower = preload("res://Scenes/Tower.tscn")
+var tower1 = preload("res://Scenes/Tower1.tscn")
 var mob = preload("res://Scenes/Enemy.tscn")
 var instance
 
@@ -9,8 +10,8 @@ var building = false
 var money = 25
 var wave = 0
 var mobs_left = 0
-var wave_mobs = [1,1,5,10,30,0]
-var wave_speed = [1.2,1,1,1,1.5,100]
+var wave_mobs = [1,1,5,10,100,0]
+var wave_speed = [1.2,1,1,1,0.1,100]
 
 func _ready():
 	$WaveTimer.start()
@@ -47,10 +48,15 @@ func _on_MobTimer_timeout():
 		else:
 			get_tree().change_scene("res://Scenes/Win.tscn")
 
-
 func _on_TextureButton_pressed():
 	if building == false and money >= 25:
 		instance = tower.instance()
+		add_child(instance)
+		building = true
+
+func _on_TextureButton1_pressed():
+	if building == false and money >= 50:
+		instance = tower1.instance()
 		add_child(instance)
 		building = true
 
