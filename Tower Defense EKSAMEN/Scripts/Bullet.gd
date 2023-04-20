7d07309802dfce2b409a4195b34261e1935754a7
hubodk <hubert2012201@gmail.com>
2023-04-20 14:44:23 +0200

    noter til hver linje

--- Tower Defense EKSAMEN/Scripts/Bullet.gd
+++ Tower Defense EKSAMEN/Scripts/Bullet.gd
@@ -1,19 +1,21 @@
 extends Area2D
 
+#overall er vector der viser hvor en specifik ting befinder sig på x og y aksen.
 
-var move = Vector2.ZERO
-var speed = 3
-var look_vec = Vector2.ZERO
-var target
+var move = Vector2.ZERO #her bliver der lavet en ny variabel der bliver kaldt "move" som fører videre til en vector som har både x og y sat til 0 altså ZERO
+var speed = 3 #her bliver der lavet en variabel som er hastigheded som er sat til 3
+var look_vec = Vector2.ZERO #"look_vec" bliver brugt til at vise hvor tårnet kigget - her bliver der defineret en variabel kaldt "look_vec" som bliver ført videre til en vector hvor både x og y aksen er sat til 0 altså ZERO#
+var target # skaber en variabel som hedder "target" uden nogen værdi men bliver blot brugt til at target fjenderne der kommer løbende
 
-func _ready():
-	if target != null:
-		$Sprite.look_at(target.global_position)
-		look_vec = target.global_position - global_position
-		
-func _physics_process(delta):
-	move = Vector2.ZERO
+func _ready(): #func _ready() bliver brugt når når en node er klar til at blive brugt i en scene.
+	if target != null: #her bliver der tjekket om "target" variablen er sat til "null" og hvis target ikke er "null" betyder det at noden har en "target" som den kigger på.
+		$Sprite.look_at(target.global_position)#target.global_position er vector hvor fjenden er #Derefter opdatere koden hvilken vej spriten kigger og den skal kigge efter "target" ved at bruge "look_at" og "look_at" bruger en vector som input som viser hvor "target" er, og target er jo fjenden
+		look_vec = target.global_position - global_position #look_vec er en 2D vector som kigger på en "target" 
+#her bliver der udregnet fra den node vi er på som er "global_position" og "target noden som er "target.global_position" som fører videre at vores vector pejer mod "target" så tårnet faktisk pejer på fjenden.
+
+func _physics_process(delta): #_physics_process(delta) er en indbygget funktion i godot som bliver brugt for at få nogen physics i spillet #delta er tiden i spillet altså frames og hvor langt skudet har flyttet sig
+	move = Vector2.ZERO #sætter vector y og x værdien til 0
 	
-	move = move.move_toward(look_vec, delta)
-	move = move.normalized() * speed
-	global_position += move
+	move = move.move_toward(look_vec, delta) #Her bliver move vektor opdateret til at anvende "move_toward" som tager den anden vector ("look_vec") som bevæger "move" vektroen til mål med den hastighed svarednde til delta
+	move = move.normalized() * speed #i denne linje bliver "move" normaliseret hvilket sætter den til 1, hvorefter den ganger move hastigheden med speed variablen fra vores tabel med forskellige speed hastighedder
+	global_position += move #Så bliver positionen af objektet opdateret ved at tilføje move vektroen til den globale position hvilket for skudet til at flytte sig
--- Tower Defense EKSAMEN/Scripts/World.gd
+++ Tower Defense EKSAMEN/Scripts/World.gd
@@ -1,62 +1,63 @@
 extends Node2D
 
-var tower = preload("res://Scenes/Tower.tscn")
-var tower1 = preload("res://Scenes/Tower1.tscn")
-var mob = preload("res://Scenes/Enemy.tscn")
-var instance
+var tower = preload("res://Scenes/Tower.tscn") #tower loader tower scenen
+var tower1 = preload("res://Scenes/Tower1.tscn") #tower1 loader tower1 scenen
+var mob = preload("res://Scenes/Enemy.tscn") #mob/fjende loader fjende scenen.
+var instance #en instance fx et dyr, men indenfor dyr kan det være hund, kat eller løve osv.
 
-var building = false
+var building = false #variabel der fortæller at man ikke kan bygge i det her område
 
-var money = 25
-var wave = 0
-var mobs_left = 0
-var wave_mobs = [1,1,5,10,100,0]
-var wave_speed = [1.2,1,1,1,0.1,100]
+var money = 25 #variabel der fortæller hvor mange penge man har tilbage, man starter på 25 penge.
+var wave = 0 #variabel der fortæller hvad runde man er på
+var mobs_left = 0 #variabel der fortæller hvor mange fjender der er tilbage
+var wave_mobs = [1,1,5,10,100,0] #tabel der viser antal fjender i hver runde, ny runde efter hver komma.
+var wave_speed = [1.2,1,1,1,0.1,100] #tabel der viser hastiheden af fjenderne
+
+#funktion er et stykke kode der kan bruges om og om igen istedet skrive det igen.
 
 func _ready():
-	$WaveTimer.start()
+	$WaveTimer.start() #funktionen er klar, og starter wave timeren
 
-func _process(delta):
-	$GUI/CashLabel.text = "Cash: " + str(money)
+func _process(delta): #_process(delta) er noget der sker hver eneste frame i spillet da spillet kører 60 frames per sekund.
+	$GUI/CashLabel.text = "Cash: " + str(money) #hver eneste frame bliver penge labelen opdateret med det antal penge man har.
+# GUI = graphical user interface
+
+func add_money(amount): #add_money er en funktion der styrer en paramter som er "amount" som er mængden af penge som bliver tilføjet til den første variabel som er money i starten af koden.
+	money += amount #penge bliver plusset med ammount ergo = alle penge samlet
 	
-func add_money(amount):
-	money += amount
-	
-func tower_built():
-	building = false
+func tower_built(): #funktionen hedder byg tårn altså tower_built at tårnet er blevet bygget
+	building = false #når funktionen bliver kaldt på så siger den at bygning bliver "false" så den ikke dur mere og den reducere penge variablen med 25, altså at man mister 25 for at bygge bygningen.
 	money -= 25
 
 
 
-func _on_WaveTimer_timeout():
-	mobs_left = wave_mobs[wave]
-	$MobTimer.wait_time = wave_speed[wave]
-	$MobTimer.start()
-	
+func _on_WaveTimer_timeout(): #funktionen hedder _on_wavetimer_timeout hvilket vil sige  det er en "callback" funktion som bliver kaldt på når en timer node der hedder "wavetimer" bliver timed out altså færdig med at tælle. Det bruger for at implementere et runde system i spillet.
+	mobs_left = wave_mobs[wave] #sætter variablen "mobs_left" til det nummer af fjender der spawner i denne runde, og den tager data fra tabellen vi har i starten af koden.
+	$MobTimer.wait_time = wave_speed[wave] #Her bliver vente tiden af en tid node som vi kalder "MobTimer" sat til hastigheden af den wave vi er nået til ud fra tabellen der er i starten af koden.
+	$MobTimer.start() #Starter timeren af noden "MobTimer" det vil gøre at timeren skyder et signal ud efter den tid der er gået som gør at der kommer flere fjender.
 
 
-
-func _on_MobTimer_timeout():
-	instance = mob.instance()
-	$Path2D.add_child(instance)
-	mobs_left -= 1
-	if mobs_left <= 0:
-		$MobTimer.stop()
-		wave +=1
-		if wave < len(wave_mobs):
-			$WaveTimer.start()
+func _on_MobTimer_timeout(): #funktionen hedder _on_mobtimer_timeout som er en funktion der bliver kaldt på når timer noden for "mobtimer" kalder. Den bliver brugt til at spawn fjender hver antal interval
+	instance = mob.instance() #laver en ny "instance" som bare er en mob altså en fjende, en instance er et træ at noder fx dyr indenfor dyr har vi (katte, hunde, løver osv.) 
+	$Path2D.add_child(instance) #den tilføjer denne nye instans af en fjende som et barn af Path2D node som betyder at barnet altid følger efter Path2D så den følger altså på path.
+	mobs_left -= 1 #Den tæller ned hvor mange fjender der skal spawnes i given runde som i tæller ned indtil alle er spawnet så den ved hvor mange den skal spawne
+	if mobs_left <= 0: #Tjekker om der er flere fjender at spawne ved at sammenligne "mobs_left" til 0, og hvis der er 0 så spawner der ikke flere.
+		$MobTimer.stop() #Hvis der ikke er flere fjender at spawne stopper "mobtimer" noden som gør at der ikke kommer flere fjender og så sætter antallet af wave op så der begynder spawne en ny wave af fjender.
+		wave +=1 
+		if wave < len(wave_mobs): #len tager bare tallet af hvad wave af fjender man er kommet til
+			$WaveTimer.start() #Hvis der er flere runder at spawne starter "wavetimer" noden igen som sender den wave vi er nået til ud så den starter.
 		else:
-			get_tree().change_scene("res://Scenes/Win.tscn")
+			get_tree().change_scene("res://Scenes/Win.tscn") #Hvis der ikke er flere waves at spawn så kommer skærmen "you win" frem hvilket betyder man har vundet.
 
-func _on_TextureButton_pressed():
-	if building == false and money >= 25:
-		instance = tower.instance()
-		add_child(instance)
-		building = true
+func _on_TextureButton_pressed(): #funktionen hedder _on_texturebutton_pressed
+	if building == false and money >= 25: #Her bliver det tjekket for om der allerede ved at blive bygget et tårn, det betyder hvis du igang med at bygge et tårn kan du ikke bygge et mere på samme tid. og hvis du har under 25 kan du ikke bygge et tårn.
+		instance = tower.instance() #laver instance som holder tårn instance, altså et træ af ting tårnet gør.
+		add_child(instance) #der bliver tilføjet et barn til instancen med tårnet
+		building = true #så når man trykker på knappen kan man godt bygge tårnet fordi den er "true"
 
-func _on_TextureButton1_pressed():
-	if building == false and money >= 50:
-		instance = tower1.instance()
-		add_child(instance)
-		building = true
+func _on_TextureButton1_pressed(): #her bliver der lavet en ny funktion som så er præcis det samme som længere oppe bare med en ny knap til et nyt slags tårn.
+	if building == false and money >= 50: #tårnet her koster 50, og hvis man ikke har mere end 50 kan man ikke bygge det
+		instance = tower1.instance() 
+		add_child(instance) #der bliver tilføjet en child note til instance af tårnet
+		building = true #bygge variablen bliver sat til "true" så nu er det muligt at bygge tårnet efter at trykke på knappen.
 
